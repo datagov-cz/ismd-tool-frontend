3af6300d7e7ed3309bd5eb06226117ba9c9a8bae
Filip Tomes <60915718+tomesfilip@users.noreply.github.com>
2025-09-21 14:13:29 +0100

    add: last visited section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,11 @@ import { MainControls } from '@/components/home/MainControls';
 import { NewsSidebar } from '@/components/home/NewsSidebar';
 import { WelcomeSection } from '@/components/home/WelcomeSection';
 import { useUserStore } from '@/store/userStore';
+import { useTranslations } from 'next-intl';
 
 export default function Home() {
+  const t = useTranslations('Home');
+
   const user = useUserStore((state) => state.user);
 
   return (
@@ -18,6 +21,11 @@ export default function Home() {
             <div className="h-[1px] my-8 bg-dark-border" />
             <WelcomeSection />
           </>
+        )}
+        {user && (
+          <div className="mt-8 space-y-5">
+            <h3 className="text-xl font-medium">{t('LastVisited.Title')}</h3>
+          </div>
         )}
       </div>
     </div>
