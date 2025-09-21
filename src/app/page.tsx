173c05eed9b1083bb0b67aae3db5bdf06f430d53
Filip Tomes <60915718+tomesfilip@users.noreply.github.com>
2025-09-21 14:07:47 +0100

    add: conditionally display parts for logged user, extra navigation buttons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,24 @@
+'use client';
+
 import { MainControls } from '@/components/home/MainControls';
 import { NewsSidebar } from '@/components/home/NewsSidebar';
 import { WelcomeSection } from '@/components/home/WelcomeSection';
+import { useUserStore } from '@/store/userStore';
 
 export default function Home() {
+  const user = useUserStore((state) => state.user);
+
   return (
     <div className="w-full max-w-desktop mx-auto px-3 xl:px-0 py-6 flex gap-x-4 gap-y-8 flex-col lg:flex-row">
       <NewsSidebar />
       <div className="mx-auto">
         <MainControls />
-        <div className="h-[1px] my-8 bg-dark-border" />
-        <WelcomeSection />
+        {!user && (
+          <>
+            <div className="h-[1px] my-8 bg-dark-border" />
+            <WelcomeSection />
+          </>
+        )}
       </div>
     </div>
   );
--- src/components/home/MainControls.tsx
+++ src/components/home/MainControls.tsx
@@ -1,3 +1,6 @@
+'use client';
+
+import { useUserStore } from '@/store/userStore';
 import {
   GovButton,
   GovFormControl,
@@ -10,6 +13,7 @@ import { useTranslations } from 'next-intl';
 
 export const MainControls = () => {
   const t = useTranslations('Home');
+  const user = useUserStore((state) => state.user);
 
   return (
     <div className="space-y-4">
@@ -21,6 +25,7 @@ export const MainControls = () => {
           {t('MainControls.CreateNewDict')}
         </GovButton>
       </div>
+      {/* TODO: Replace with custom search: search is not triggering any event */}
       <GovFormControl className="w-full">
         <GovFormGroup>
           <GovFormSearch>
@@ -34,6 +39,22 @@ export const MainControls = () => {
           </GovFormSearch>
         </GovFormGroup>
       </GovFormControl>
+      {user && (
+        <div className="flex gap-4 flex-wrap justify-center">
+          <GovButton type="solid" size="m" color="primary">
+            {t('MainControls.Logged.DraftDictionaries')}
+          </GovButton>
+          <GovButton type="solid" size="m" color="primary">
+            {t('MainControls.Logged.PublishedDictionaries')}
+          </GovButton>
+          <GovButton type="solid" size="m" color="primary">
+            {t('MainControls.Logged.Definitions')}
+          </GovButton>
+          <GovButton type="solid" size="m" color="primary">
+            {t('MainControls.Logged.Diagrams')}
+          </GovButton>
+        </div>
+      )}
     </div>
   );
 };
