a6992690c74dcf368fc600f28bf1aa8ae4aa80b3
Filip Tomes <60915718+tomesfilip@users.noreply.github.com>
2025-09-24 15:40:45 +0100

    add: basic layout for dictionary detail

---
+++ src/app/dictionary/[slug]/page.tsx
@@ -0,0 +1,67 @@
+import { GovIcon } from '@gov-design-system-ce/react';
+import { useTranslations } from 'next-intl';
+
+import { GridContainer } from '@/components/dictionaryDetail/GridContainer';
+import { SidebarContainer } from '@/components/shared/SidebarContainer';
+
+const DictionaryDetail = () => {
+  const t = useTranslations('DictionaryDetail');
+
+  const isDraft = true; // TODO: get real status from API
+
+  return (
+    <>
+      <SidebarContainer>
+        {/* TODO: Make custom Searchbar component */}
+        <div className="flex items-center gap-2 border border-dark-border rounded-lg px-4 py-3.5 relative">
+          <GovIcon name="search" size="m" slot="icon-start" className="z-[2]" />
+          <label htmlFor="search-diagrams" className="hidden">
+            {t('Sidebar.SearchbarPlaceholder')}
+          </label>
+          <input
+            type="text"
+            id="search-diagrams"
+            className="w-full absolute top-0 left-0 h-full rounded-lg pl-10 pr-4 z-[1] outline-none text-sm"
+            placeholder={t('Sidebar.SearchbarPlaceholder')}
+          />
+        </div>
+      </SidebarContainer>
+      <div className="w-full px-4 space-y-6">
+        <GridContainer>
+          <div className="space-y-2 col-span-4 col-start-2">
+            <h1 className="text-xl lg:text-3xl font-bold">Slovník 360/2023</h1>
+            <p className="text-sm text-dark-secondary">
+              {isDraft
+                ? t('Main.DictionaryStatus.Draft')
+                : t('Main.DictionaryStatus.Published')}
+            </p>
+          </div>
+        </GridContainer>
+        <GridContainer>
+          <p className="font-medium text-xl">
+            {t('Main.Sections.Description')}
+          </p>
+          <p className="col-span-4">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero id
+            minima voluptate molestias quae omnis voluptatem consequuntur,
+            exercitationem pariatur ipsa.
+          </p>
+          <p className="font-medium text-xl">
+            {t('Main.Sections.RelatedResources')}
+          </p>
+          <p className="col-span-4">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione
+            architecto autem asperiores molestias perspiciatis hic inventore
+            consequuntur qui mollitia velit?
+          </p>
+        </GridContainer>
+        <GridContainer>
+          <p className="font-medium text-xl">{t('Main.Sections.Terms')}</p>
+          <div className="col-span-4"></div>
+        </GridContainer>
+      </div>
+    </>
+  );
+};
+
+export default DictionaryDetail;
--- src/components/home/NewsSidebar.tsx
+++ src/components/home/NewsSidebar.tsx
@@ -1,6 +1,7 @@
 import { useTranslations } from 'next-intl';
 
 import { tempArticles } from '@/lib/constants';
+import { SidebarContainer } from '../shared/SidebarContainer';
 
 import { NewsArticle } from './NewsArticle';
 
@@ -8,7 +9,7 @@ export function NewsSidebar() {
   const t = useTranslations('Home');
 
   return (
-    <aside className="lg:border-r lg:border-b-0 border-b border-secondary lg:pr-4 pb-6 lg:pb-0 space-y-6 w-full lg:max-w-[300px]">
+    <SidebarContainer>
       <h3 className="font-medium text-lg lg:text-xl">{t('News.Title')}</h3>
       <div className="space-y-4 lg:space-y-6">
         {tempArticles.map((article) => (
@@ -21,6 +22,6 @@ export function NewsSidebar() {
           />
         ))}
       </div>
-    </aside>
+    </SidebarContainer>
   );
 }
