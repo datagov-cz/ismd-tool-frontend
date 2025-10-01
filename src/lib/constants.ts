88b55caeadcc66f0dbd8e20872ac5edbb9a22e4a
Filip Tomes <60915718+tomesfilip@users.noreply.github.com>
2025-10-01 16:28:12 +0100

    add: missing constants

---
+++ src/lib/constants.ts
@@ -0,0 +1,52 @@
+// TODO: Remove this file when real articles are available -> create MDX file support
+export const tempArticles = [
+  {
+    id: 1,
+    date: '2.5.2025',
+    title: 'Nová funkce pro správu slovníků',
+    description:
+      'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laudantium, totam ab commodi ut veniam similique laboriosam enim odit quaerat cupiditate?',
+    href: '/',
+  },
+  {
+    id: 2,
+    date: '28.4.2025',
+    title: 'Aktualizace uživatelského rozhraní',
+    description:
+      'Vylepšili jsme uživatelské rozhraní pro lepší přehlednost a snadnější používání aplikace.',
+    href: '/',
+  },
+];
+
+export const tempDraftDictionaries = [
+  {
+    id: 1,
+    title: 'Slovník 360/2023',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fringilla at massa nec luctus. Vestibulum mi est, varius et imperdiet vitae, fermentum eget ante.',
+  },
+  {
+    id: 2,
+    title: 'Slovník 361/2023',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fringilla at massa nec luctus. Vestibulum mi est, varius et imperdiet vitae, fermentum eget ante.',
+  },
+  {
+    id: 3,
+    title: 'Slovník 362/2023',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fringilla at massa nec luctus. Vestibulum mi est, varius et imperdiet vitae, fermentum eget ante.',
+  },
+  {
+    id: 4,
+    title: 'Slovník 363/2023',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fringilla at massa nec luctus. Vestibulum mi est, varius et imperdiet vitae, fermentum eget ante.',
+  },
+  {
+    id: 5,
+    title: 'Slovník 364/2023',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fringilla at massa nec luctus. Vestibulum mi est, varius et imperdiet vitae, fermentum eget ante.',
+  },
+  {
+    id: 6,
+    title: 'Slovník 365/2023',
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi fringilla at massa nec luctus. Vestibulum mi est, varius et imperdiet vitae, fermentum eget ante.',
+  },
+];
