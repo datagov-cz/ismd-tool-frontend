0162b7e74417756a5f57246700e9dd58d6f5ae36
Filip Tomes <60915718+tomesfilip@users.noreply.github.com>
2025-10-18 23:48:10 +0100

    add: crossed line when offline

--- src/components/header/OnlineIndicator.tsx
+++ src/components/header/OnlineIndicator.tsx
@@ -21,7 +21,9 @@ export const OnlineIndicator = () => {
         aria-label={title}
         className={`${isOnline ? 'text-green' : 'text-gray'} transition-colors duration-150`}
       />
-
+      <div
+        className={`absolute top-0 left-1/2 -translate-x-1/2 w-[2px] rotate-45 dark:bg-white bg-dark-border h-full transition-opacity duration-300 ${isOnline ? 'opacity-0' : 'opacity-100'}`}
+      />
       <span
         role="tooltip"
         className="pointer-events-none absolute left-1/2 -bottom-6 z-[2] w-max max-w-xs -translate-x-1/2 rounded bg-blue text-white text-xs py-1 px-2 opacity-0 group-hover:opacity-100 group-focus:opacity-100 transition-opacity duration-150"
