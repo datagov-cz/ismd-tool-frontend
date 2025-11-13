1ed15ca42ae8a50fdb98eb5a7bb74c69e8405c1d
Filip Tomes <filiptomes@protonmail.com>
2025-11-13 10:00:32 +0000

    add: setup nextauth with keycloack as a provider

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,33 @@
+import NextAuth from 'next-auth';
+import KeycloakProvider from 'next-auth/providers/keycloak';
+
+const handler = NextAuth({
+  providers: [
+    KeycloakProvider({
+      clientId: process.env.KEYCLOAK_CLIENT_ID!,
+      clientSecret: process.env.KEYCLOAK_CLIENT_SECRET!,
+      issuer: process.env.KEYCLOAK_ISSUER!,
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, account }) {
+      if (account) {
+        token.accessToken = account.access_token;
+        token.idToken = account.id_token;
+        token.refreshToken = account.refresh_token;
+        token.expiresAt = account.expires_at;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      session.accessToken = token.accessToken as string;
+      session.error = token.error as string;
+      return session;
+    },
+  },
+  session: {
+    strategy: 'jwt',
+  },
+});
+
+export { handler as GET, handler as POST };
