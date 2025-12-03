6bd963456ff7bf350b696ae1b9892322c4706e50
Filip Tomes <filiptomes@protonmail.com>
2025-12-03 13:45:40 +0000

    add: fix next config errors, setup basic pwa, cache the pages, eslint new version migration

---
+++ eslint.config.mjs
@@ -0,0 +1,51 @@
+import { defineConfig, globalIgnores } from 'eslint/config';
+import nextVitals from 'eslint-config-next/core-web-vitals';
+import nextTs from 'eslint-config-next/typescript';
+import prettier from 'eslint-config-prettier/flat';
+import reactHooks from 'eslint-plugin-react-hooks';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
+import unusedImports from 'eslint-plugin-unused-imports';
+
+const eslintConfig = defineConfig([
+  ...nextVitals,
+  ...nextTs,
+  prettier,
+  globalIgnores([
+    '.next/**',
+    'out/**',
+    'build/**',
+    'next-env.d.ts',
+    'src/api/generated.ts',
+  ]),
+  {
+    plugins: {
+      'simple-import-sort': simpleImportSort,
+      'unused-imports': unusedImports,
+      'react-hooks': reactHooks,
+    },
+    rules: {
+      'no-unused-vars': 'warn',
+      'no-console': 'warn',
+      'no-redeclare': 'warn',
+      'unused-imports/no-unused-imports': 'error',
+
+      'react-hooks/set-state-in-effect': 'warn',
+
+      'simple-import-sort/imports': [
+        'error',
+        {
+          groups: [
+            ['^\\u0000'],
+            ['^react$', '^@?\\w'],
+            ['^@', '^'],
+            ['^\\./'],
+            ['^.+\\.(module.css|module.scss)$'],
+            ['^.+\\.(gif|png|svg|jpg)$'],
+          ],
+        },
+      ],
+    },
+  },
+]);
+
+export default eslintConfig;
