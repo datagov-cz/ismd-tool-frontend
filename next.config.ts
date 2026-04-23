c9e73332abae11e093f425e73f06740743712277
nicolas.bertelsen <stilas.bertelsen@gmail.com>
2026-04-23 16:27:56 +0200

    development: add rewrite for swagger api docs - add frontend rewrites to direct to backend for swagger api docs

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,23 @@ const nextConfig: NextConfig = {
   experimental: {
     mdxRs: true,
   },
+  async rewrites() {
+    const beUrl = process.env.BE_URL ?? '';
+    return [
+      { source: '/v3/api-docs', destination: `${beUrl}/v3/api-docs` },
+      {
+        source: '/v3/api-docs/:path*',
+        destination: `${beUrl}/v3/api-docs/:path*`,
+      },
+      { source: '/swagger-ui', destination: `${beUrl}/swagger-ui` },
+      {
+        source: '/swagger-ui/:path*',
+        destination: `${beUrl}/swagger-ui/:path*`,
+      },
+      { source: '/api-docs', destination: `${beUrl}/api-docs` },
+      { source: '/api-docs/:path*', destination: `${beUrl}/api-docs/:path*` },
+    ];
+  },
 };
 
 const withMDX = createMDX({
