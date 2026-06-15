9dc5f28e9fecf0d5d40e946da1814aead2c24435
Jakub Kolebaba <jakub@kolebaba.cz>
2026-06-15 14:02:15 +0200

    f/DI-331 - Law autocomplete

--- src/components/conceptForm/sections/SourcesSection.tsx
+++ src/components/conceptForm/sections/SourcesSection.tsx
@@ -1,12 +1,22 @@
 import { useTranslations } from 'next-intl';
 
+import { LegislativeSourceInput } from '@/components/shared/LegislativeSourceInput';
 import { NonLegislativeSourceInput } from '@/components/shared/NonLegislativeSourceInput';
 import { FormSection } from '../components/FormSection';
 
 export const SourcesSection = () => {
   const t = useTranslations('ConceptDetail.Sections');
+
   return (
     <FormSection label={t('ConceptSource')} icon="book">
+      <LegislativeSourceInput
+        label="Definující ustanovení právního předpisu"
+        name="a"
+      />
+      <LegislativeSourceInput
+        label="Související ustanovení právního předpisu"
+        name="b"
+      />
       <NonLegislativeSourceInput
         label={t('DefiningNonLegislative')}
         name="definingNonLegalSource"
